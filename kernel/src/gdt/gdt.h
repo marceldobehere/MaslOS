773ce2f3436c34e318d27635c64837098b57319e
Marceldobehere <marcel.z2006@gmail.com>
2022-02-11 21:51:41 +0100

    nasm go brrt

--- kernel/src/gdt/gdt.h
+++ kernel/src/gdt/gdt.h
@@ -3,8 +3,46 @@
 
 struct GDTDescriptor
 {
+    uint16_t Size;
+    uint64_t Offset;
+
+
+}__attribute((packed));
+
+
+struct GDTEntry 
+{
+    uint16_t Limit0;
+    uint16_t Base0;
+    uint8_t Base1;
+    uint8_t AccessByte;
+    uint8_t Limit1_Flags;
+    uint8_t Base2;
+}__attribute((packed));
+
+
+struct GDT
+{
+    GDTEntry Null;
+    GDTEntry KernelCode;
+    GDTEntry KernelData;
+    GDTEntry UserNull;
+    GDTEntry UserCode;
+    GDTEntry UserData;
+
+}__attribute((packed))
+__attribute((alligned(0x1000)));
+
+
+GDT DefaultGDT =
+{
+    {0, 0, 0, 0x00, 0x00, 0}, // null
+    {0, 0, 0, 0x9a, 0xa0, 0}, // kernel code segment
+    {0, 0, 0, 0x92, 0xa0, 0}, // kernel data segment
+    {0, 0, 0, 0x00, 0x00, 0}, // user null
+    {0, 0, 0, 0x9a, 0xa0, 0}, // user code segment
+    {0, 0, 0, 0x92, 0xa0, 0}  // user data segment
 
 
 };
-
 
