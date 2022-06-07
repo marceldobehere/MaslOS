1fb1407eafa6eca3c52f934c33d69dd30c13030c
Marceldobehere <marcel.z2006@gmail.com>
2022-06-07 08:27:57 +0200

    z

--- kernel/src/userinput/mouse.cpp
+++ kernel/src/userinput/mouse.cpp
@@ -1,18 +1,6 @@
 #include "mouse.h"
   
 
-<<<<<<< HEAD
-#define PS2XSign 0b00010000
-#define PS2YSign 0b00100000
-#define PS2XOverflow 0b01000000
-#define PS2YOverflow 0b10000000
-
-#define PS2LeftButton   0b00000001
-#define PS2MiddleButton 0b00000010
-#define PS2RightButton  0b00000100
-
-Point MousePosition;
-=======
 #define PS2XSign        0b00010000
 #define PS2YSign        0b00100000
 #define PS2XOverflow    0b01000000
@@ -174,7 +162,6 @@ void DrawMousePointer()
 }
 
 
->>>>>>> 920d0ddba31fc5e80ed8b9ba51c8cf300a2ba1ab
 
 void Mousewait()
 {
