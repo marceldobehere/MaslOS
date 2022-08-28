68b6e33b5a6430d1ef7d6059b5f740399f5b56b1
Marceldobehere <marcel.z2006@gmail.com>
2022-08-28 03:48:17 +0200

    changed file

--- Just RUN OS.sh
+++ Just RUN OS.sh
@@ -2,4 +2,4 @@
 
 cd kernel
 
-qemu-system-x86_64 -machine q35 -drive file=bin/MaslOS.img -m 256M -cpu qemu64 -drive if=pflash,format=raw,unit=0,file="../ovmfbin/OVMF_CODE-pure-efi.fd",readonly=on -drive if=pflash,format=raw,unit=1,file="../ovmfbin/OVMF_VARS-pure-efi.fd" -net none
+qemu-system-x86_64 -machine q35 -drive file=bin/MaslOS.img -drive file=bin/blank.img -m 1G -cpu qemu64 -drive if=pflash,format=raw,unit=0,file="../ovmfbin/OVMF_CODE-pure-efi.fd",readonly=on -drive if=pflash,format=raw,unit=1,file="../ovmfbin/OVMF_VARS-pure-efi.fd" -net none
