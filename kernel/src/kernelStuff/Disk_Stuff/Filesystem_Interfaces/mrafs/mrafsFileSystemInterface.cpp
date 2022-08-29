d4b46faf03dd0a16616b838230174b7f1c656320
Marceldobehere <marcel.z2006@gmail.com>
2022-08-29 23:51:34 +0200

    more stuff

--- kernel/src/kernelStuff/Disk_Stuff/Filesystem_Interfaces/mrafs/mrafsFileSystemInterface.cpp
+++ kernel/src/kernelStuff/Disk_Stuff/Filesystem_Interfaces/mrafs/mrafsFileSystemInterface.cpp
@@ -10,102 +10,243 @@ namespace FilesystemInterface
         this->partitionInfo = partitionInfo;
     }
 
+
     const char* MrafsFilesystemInterface::CreateFile(const char* path)
     {
+        if (partitionInterface == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION_INTERFACE;
+        if (partitionInfo == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION;
+        if (partitionInfo->owner != partitionInterface)
+            return FSCommandResult.ERROR_INVALID_PARTITION_OWNER;
+
+
         return FSCommandResult.ERROR_FUNCTION_NOT_IMPLEMENTED;
     }
     
     const char* MrafsFilesystemInterface::CreateFolder(const char* path)
     {
+        if (partitionInterface == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION_INTERFACE;
+        if (partitionInfo == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION;
+        if (partitionInfo->owner != partitionInterface)
+            return FSCommandResult.ERROR_INVALID_PARTITION_OWNER;
+
+
         return FSCommandResult.ERROR_FUNCTION_NOT_IMPLEMENTED;
     }
 
 
     const char* MrafsFilesystemInterface::DeleteFile(const char* path)
     {
+        if (partitionInterface == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION_INTERFACE;
+        if (partitionInfo == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION;
+        if (partitionInfo->owner != partitionInterface)
+            return FSCommandResult.ERROR_INVALID_PARTITION_OWNER;
+
+
         return FSCommandResult.ERROR_FUNCTION_NOT_IMPLEMENTED;
     }
 
     const char* MrafsFilesystemInterface::DeleteFolder(const char* path)
     {
+        if (partitionInterface == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION_INTERFACE;
+        if (partitionInfo == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION;
+        if (partitionInfo->owner != partitionInterface)
+            return FSCommandResult.ERROR_INVALID_PARTITION_OWNER;
+
+
         return FSCommandResult.ERROR_FUNCTION_NOT_IMPLEMENTED;
     }
 
 
     const char* MrafsFilesystemInterface::CopyFile(const char* oldPath, const char* newPath)
     {
+        if (partitionInterface == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION_INTERFACE;
+        if (partitionInfo == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION;
+        if (partitionInfo->owner != partitionInterface)
+            return FSCommandResult.ERROR_INVALID_PARTITION_OWNER;
+
+
         return FSCommandResult.ERROR_FUNCTION_NOT_IMPLEMENTED;
     }
 
     const char* MrafsFilesystemInterface::CopyFolder(const char* oldPath, const char* newPath)
     {
+        if (partitionInterface == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION_INTERFACE;
+        if (partitionInfo == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION;
+        if (partitionInfo->owner != partitionInterface)
+            return FSCommandResult.ERROR_INVALID_PARTITION_OWNER;
+
+
         return FSCommandResult.ERROR_FUNCTION_NOT_IMPLEMENTED;
     }
 
 
     const char* MrafsFilesystemInterface::RenameFile(const char* oldPath, const char* newPath)
     {
+        if (partitionInterface == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION_INTERFACE;
+        if (partitionInfo == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION;
+        if (partitionInfo->owner != partitionInterface)
+            return FSCommandResult.ERROR_INVALID_PARTITION_OWNER;
+
+
         return FSCommandResult.ERROR_FUNCTION_NOT_IMPLEMENTED;
     }
 
     const char* MrafsFilesystemInterface::RenameFolder(const char* oldPath, const char* newPath)
     {
+        if (partitionInterface == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION_INTERFACE;
+        if (partitionInfo == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION;
+        if (partitionInfo->owner != partitionInterface)
+            return FSCommandResult.ERROR_INVALID_PARTITION_OWNER;
+
+
         return FSCommandResult.ERROR_FUNCTION_NOT_IMPLEMENTED;
     }
 
 
     bool MrafsFilesystemInterface::FileExists(const char* path)
     {
+        if (partitionInterface == NULL)
+            return false;
+        if (partitionInfo == NULL)
+            return false;
+        if (partitionInfo->owner != partitionInterface)
+            return false;
+
+
         return false;
     }
 
     bool MrafsFilesystemInterface::FolderExists(const char* path)
     {
+        if (partitionInterface == NULL)
+            return false;
+        if (partitionInfo == NULL)
+            return false;
+        if (partitionInfo->owner != partitionInterface)
+            return false;
+
         return false;
     }
 
 
     FileInfo* MrafsFilesystemInterface::GetFileInfo(const char* path)
     {
+        if (partitionInterface == NULL)
+            return NULL;
+        if (partitionInfo == NULL)
+            return NULL;
+        if (partitionInfo->owner != partitionInterface)
+            return NULL;
+            
         return NULL;
     }
 
     FolderInfo* MrafsFilesystemInterface::GetFolderInfo(const char* path)
     {
+        if (partitionInterface == NULL)
+            return NULL;
+        if (partitionInfo == NULL)
+            return NULL;
+        if (partitionInfo->owner != partitionInterface)
+            return NULL;
+
         return NULL;
     }
 
 
     const char** MrafsFilesystemInterface::GetFiles(const char* path)
     {
+        if (partitionInterface == NULL)
+            return NULL;
+        if (partitionInfo == NULL)
+            return NULL;
+        if (partitionInfo->owner != partitionInterface)
+            return NULL;
+
+
         return NULL;
     }
 
 
     const char* MrafsFilesystemInterface::ReadFile(const char* path, void** buffer)
     {
+        if (partitionInterface == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION_INTERFACE;
+        if (partitionInfo == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION;
+        if (partitionInfo->owner != partitionInterface)
+            return FSCommandResult.ERROR_INVALID_PARTITION_OWNER;
+
+
         return FSCommandResult.ERROR_FUNCTION_NOT_IMPLEMENTED;
     }
 
     const char* MrafsFilesystemInterface::WriteFile(const char* path, void** buffer)
     {
+        if (partitionInterface == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION_INTERFACE;
+        if (partitionInfo == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION;
+        if (partitionInfo->owner != partitionInterface)
+            return FSCommandResult.ERROR_INVALID_PARTITION_OWNER;
+
+
         return FSCommandResult.ERROR_FUNCTION_NOT_IMPLEMENTED;
     }
 
 
     const char* MrafsFilesystemInterface::ReadFile(const char* path, uint64_t byteCount, void* buffer)
     {
+        if (partitionInterface == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION_INTERFACE;
+        if (partitionInfo == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION;
+        if (partitionInfo->owner != partitionInterface)
+            return FSCommandResult.ERROR_INVALID_PARTITION_OWNER;
+
+
         return FSCommandResult.ERROR_FUNCTION_NOT_IMPLEMENTED;
     }
 
     const char* MrafsFilesystemInterface::WriteFile(const char* path, uint64_t byteCount, void* buffer)
     {
+        if (partitionInterface == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION_INTERFACE;
+        if (partitionInfo == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION;
+        if (partitionInfo->owner != partitionInterface)
+            return FSCommandResult.ERROR_INVALID_PARTITION_OWNER;
+
+
         return FSCommandResult.ERROR_FUNCTION_NOT_IMPLEMENTED;
     }
 
 
     const char* MrafsFilesystemInterface::SaveFSTable()
     {
+        if (partitionInterface == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION_INTERFACE;
+        if (partitionInfo == NULL)
+            return FSCommandResult.ERROR_NO_PARTITION;
+        if (partitionInfo->owner != partitionInterface)
+            return FSCommandResult.ERROR_INVALID_PARTITION_OWNER;
+
         return FSCommandResult.ERROR_FUNCTION_NOT_IMPLEMENTED;
     }
 
