dc7a18252b51bc31f1a95288d6e42a2fd08227d0
Marceldobehere <marcel.z2006@gmail.com>
2023-01-29 00:02:14 +0100

    a

--- run.cmd
+++ run.cmd
@@ -1,1 +1,1 @@
-qemu-system-x86_64 -machine q35 -m 1G -cpu qemu64 -drive file=disk.img -boot d -cdrom barebones.iso
+qemu-system-x86_64 -machine q35 -m 1G -cpu qemu64 -drive file=disk.img -L "C:\Program Files\qemu" -boot d -cdrom barebones.iso
